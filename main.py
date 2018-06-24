a69e7cfd062e00ca0927ac72038c59dcfaa3e705
hejack0207 <hejack0207@sina.com>
2018-06-24 22:52:34 +0800

    using pipenv,dos2unix,iconv encoding from gbk to utf8

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import random
 from pypinyin import lazy_pinyin
+
 def idiom_exists(CY):  #判断成语是否存在
-    with open('.\idiom.txt','r') as f:
+    with open('idiom.txt','r') as f:
         for i in set(f.readlines()):
             if CY ==i.strip():
                 return True
         return False
 
 def idiom_select(x):                               #参数为x的成语，返回该成语的接龙匹配成语
-        with open('.\idiom.txt','r') as f:
+        with open('idiom.txt','r') as f:
             base = f.readlines()
             random.shuffle(base)
             for i in base:
-                if i[:-1] == x or len(i)  != 5:
+                if i[:-1] == x or len(i) != 4:
                     continue
                 if lazy_pinyin(i[0])==lazy_pinyin(x[-1]):
                     return i[:-1]
@@ -26,6 +27,3 @@ def main():
             print("该成语不存在，结束")
             break
 main()
-
-
-
