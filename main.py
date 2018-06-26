ca8298e7a57f0f9983502802de8515a9c2378ed9
hejack0207 <hejack0207@sina.com>
2018-06-26 14:05:57 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 from pypinyin import lazy_pinyin
 
+#tongyin=[]
+#tongxing=[]
+
 def idiom_exists(CY): 
     with open('idiom.txt','r') as f:
         for i in set(f.readlines()):
@@ -9,20 +12,27 @@ def idiom_exists(CY):
         return False
 
 def idiom_select(x): 
-        with open('idiom.txt','r') as f:
-            base = f.readlines()
-            random.shuffle(base)
-            for i in base:
-                if i[:-1] == x or len(i) != 5:
-                    continue
-                if lazy_pinyin(i[0])==lazy_pinyin(x[-1]):
-                    return i[:-1]
+    tongyin=[]
+    tongxing=[]
+    with open('idiom.txt','r') as f:
+        base = f.readlines()
+        random.shuffle(base)
+        for i in base:
+            if i[:-1] == x or len(i) != 5:
+                continue
+            if i[0]==x[-1]:
+                tongxing.append(i[:-1])
+                continue
+            if lazy_pinyin(i[0])==lazy_pinyin(x[-1]):
+                tongyin.append(i[:-1])
+    return tongxing, tongyin
 
 def main():
     while True:
         x = input("请输入：")
         if idiom_exists(x):
-            print(idiom_select(x))
+            tongxing, tongyin = idiom_select(x)
+            print('\n'.join(tongxing))
         else:
             print("不存在，结束")
             break
