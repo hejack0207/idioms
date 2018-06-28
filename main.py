01f693ebaa351e5480d662f24916a27d62bc771a
hejack0207 <hejack0207@sina.com>
2018-06-28 21:14:27 +0800

    fix

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import random
 from pypinyin import lazy_pinyin
+import fire
 
-#tongyin=[]
-#tongxing=[]
-
-def idiom_exists(CY): 
+def _idiom_exists(CY): 
     with open('idiom.txt','r') as f:
         for i in set(f.readlines()):
             if CY ==i.strip():
                 return True
         return False
 
-def idiom_select(x): 
+def _idiom_select(x): 
     tongyin=[]
     tongxing=[]
     with open('idiom.txt','r') as f:
@@ -27,16 +25,44 @@ def idiom_select(x):
                 tongyin.append(i[:-1])
     return tongxing, tongyin
 
-def main():
+def shou_wei_zi(): 
+    sz = dict()
+    wz = dict()
+    with open('idiom.txt','r') as f:
+        for l in set(f.readlines()):
+            s,w = l[0],l[-2]
+            if s in sz:
+                sz[s]=sz[s]+1
+            else:
+                sz[s]=1
+            if w in wz:
+                wz[w]=wz[w]+1
+            else:
+                wz[w]=1
+
+        sz_sorted = sorted(sz.items(), key=lambda i:i[1]) 
+        wz_sorted = sorted(wz.items(), key=lambda i:i[1]) 
+        for k,v in sz_sorted:
+            print("{0:s},{1:d}".format(k,v))
+
+        print(len(sz))
+        szs,wzs=set(sz.keys()),set(wz.keys())
+        print("{} in both,{} only in shouzi,{} in weizi".format(len(szs & wzs),len(szs - wzs), len(wzs - szs)))
+
+
+def interactive():
     while True:
         try:
             x = input()
-            if idiom_exists(x):
-                tongxing, tongyin = idiom_select(x)
+            if _idiom_exists(x):
+                tongxing, tongyin = _idiom_select(x)
                 print('\n'.join(tongxing))
             else:
                 print("不存在，结束")
         except EOFError as error:
             break
+
+def main():
+    fire.Fire()
 
 main()
